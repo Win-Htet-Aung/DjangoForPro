4b630b5a605cc1505489092e22e3a8e2f5ac059b
Win Htet Aung <whaung1998@gmail.com>
2022-07-13 20:15:21 +0630

    testing pre-commit

--- school/models.py
+++ school/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Teacher(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=110)
     dob = models.DateField()
 
     def __str__(self):
         return f"Tr. {self.name}"
 
 class Subject(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=110)
     grade = models.PositiveIntegerField()
     teacher = models.ManyToManyField(Teacher)
 
